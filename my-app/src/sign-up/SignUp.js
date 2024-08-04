4c11760007cf30265f076c7dfbd20bf0974bbe96
Yug Patil <156501997+YugKp44@users.noreply.github.com>
2024-08-04 17:51:19 +0530

    Update SignUp.js

--- my-app/src/sign-up/SignUp.js
+++ my-app/src/sign-up/SignUp.js
@@ -47,7 +47,7 @@ export const SignUp = () => {
       setLoading(true);  // Show loading animation
 
       try {
-         const url = "http://localhost:8000/auth/signup";
+         const url = "https://login-signup-backend-self.vercel.app/auth/signup";
          const response = await fetch(url, {
             method: 'POST',
             headers: {
